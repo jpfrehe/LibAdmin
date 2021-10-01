a726cc2b8493b9dac9c904bf5fe24b62e5223025
Jan Philip <Frehe>
2021-10-01 10:32:17 +0200

    Created Spring Boot Project

---
+++ src/main/java/de/hswhameln/isbnvalidator/IsbnvalidatorApplication.java
@@ -0,0 +1,13 @@
+package de.hswhameln.isbnvalidator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsbnvalidatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IsbnvalidatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/hswhameln/isbnvalidator/IsbnvalidatorApplicationTests.java
@@ -0,0 +1,13 @@
+package de.hswhameln.isbnvalidator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsbnvalidatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
