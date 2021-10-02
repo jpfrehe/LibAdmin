9d1e68c3cf3048718094557afc71774cab41a847
Matzel0307 <marcel-karau@gmx.de>
2021-10-02 12:46:13 +0200

    Pimp Up of First TestSet

--- src/test/java/de/hswhameln/isbnvalidator/IsbnvalidatorApplicationTests.java
+++ src/test/java/de/hswhameln/isbnvalidator/IsbnvalidatorApplicationTests.java
@@ -1,7 +1,9 @@
 package de.hswhameln.isbnvalidator;
 
 import de.hswhameln.isbnvalidator.beans.Book;
+import de.hswhameln.isbnvalidator.exceptions.BookNotFoundException;
 import de.hswhameln.isbnvalidator.services.BookService;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,6 +12,8 @@ import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
 import java.util.ListIterator;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SpringBootTest
 class IsbnvalidatorApplicationTests {
@@ -27,12 +31,19 @@ class IsbnvalidatorApplicationTests {
 
 	@Test
 	void deleteBookTest() {
+		service.deleteBooks(List.of(new Book("Nina & Tom","Tom Kummer","Blumenbar","978-3-351-05035-1")));
+	}
+
+	@Disabled @Test
+	void deleteBookAndSearchTest() {
 		service.deleteBooks(List.of(new Book("Es existiert","Johannes Huber","Goldmann","978-3-442-22232-2")));
+		assertThrows(BookNotFoundException.class, () -> service.findBook("978-3-442-22232-2").get());
 	}
 
 	@Test
 	void createBookTest() {
 		service.createBook(new Book("Star Wars: Das Licht der Jedi", "Charles Soule","Blanvalent","978-0-593-15771-8"));
+		System.out.println(service.findBook("978-0-593-15771-8").get().getTitle());
 	}
 
 	@Test
@@ -40,9 +51,22 @@ class IsbnvalidatorApplicationTests {
 		service.findBook("978-3-442-22232-2");
 	}
 
-	@Test
+	@Disabled @Test
 	void createFalseBookTest() {
 		service.createBook(new Book("Star Wars: Das Licht der Jedi", "Charles Soule","Blanvalent","978-0-593-15771-9"));
 		System.out.println(service.findBook("978-0-593-15771-9").get().getTitle());
+	}
+
+	@Test
+	void createAndDeleteTest() {
+		Book b = new Book("SuperSache","Markus Büning", "Panini", "978-3-442-12345-2");
+		service.createBook(b);
+		service.deleteBooks(List.of(b));
+	}
+
+	@Disabled @Test
+	void findAndDeleteTest() {
+		// Book b = new Book(service.findBook("978-3-7371-0140-0"));
+		// service.deleteBooks(List.of(b));
 	}
 }
