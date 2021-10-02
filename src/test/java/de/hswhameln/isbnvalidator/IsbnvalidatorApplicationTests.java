044a4d650e3702f34ecac77b77bea943c45a7171
Matzel0307 <marcel-karau@gmx.de>
2021-10-02 11:44:16 +0200

    First Tests

--- src/test/java/de/hswhameln/isbnvalidator/IsbnvalidatorApplicationTests.java
+++ src/test/java/de/hswhameln/isbnvalidator/IsbnvalidatorApplicationTests.java
@@ -1,9 +1,15 @@
 package de.hswhameln.isbnvalidator;
 
+import de.hswhameln.isbnvalidator.beans.Book;
 import de.hswhameln.isbnvalidator.services.BookService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
 
 @SpringBootTest
 class IsbnvalidatorApplicationTests {
@@ -17,6 +23,16 @@ class IsbnvalidatorApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void deleteBookTest() {
+		service.deleteBooks(List.of(new Book("Es existiert","Johannes Huber","Goldmann","978-3-442-22232-2")));
+	}
+
+	@Test
+	void createBookTest() {
+		service.createBook(new Book("Star Wars: Das Licht der Jedi", "Charles Soule","Blanvalent","978-0-593-15771-8"));
 	}
 
 }
